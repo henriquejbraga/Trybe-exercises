a31784acc1bc51478dfc54638c9e5fc2d55464fb
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-06-25 18:30:48 -0300

    3

--- bloco_4/dia_3/exercicios_dia_3.js
+++ bloco_4/dia_3/exercicios_dia_3.js
@@ -1,6 +1,6 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
 
-let simbolo = 'x';
+let simbolo = '*';
 let n = 5;
 let espaço = '';
 
@@ -14,10 +14,30 @@ for(let i = 0; i < n; i+= 1){
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
 let n = 5;
-let simbolo = 'x';
+let simbolo = '*';
 let espaço = '';
 
 for(let i = 0; i < n ;i +=1){
   espaço = simbolo + espaço;
   console.log(espaço);
+}
+
+// 3- Agora inverta o lado do triângulo.
+
+let n = 5;
+let simbolo = '*';
+let espaço = '';
+let base = n;
+
+for (let i = 0; i < n; i +=1){
+  for(let j = 0; j <= n; j +=1){
+    if(j < base){
+      espaço = espaço + ' ';
+    } else{
+      espaço = espaço + simbolo;
+    }
+  }
+  console.log(espaço);
+  espaço = '';
+  base -= 1;
 }
